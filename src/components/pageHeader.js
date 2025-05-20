5d16d33f69c0dd47fdee4396849dd2073f4ff8c2
ZachMc21 <magloughlin.zach@gmail.com>
2025-05-19 18:29:03 -0600

    Working on adding header/jumbotron and styling it.

---
+++ src/components/pageHeader.js
@@ -0,0 +1,13 @@
+import '../scss/pageHeader.scss';
+
+function pageHeader() {
+    return(
+        <div className="page-header">
+            <h1>Zach Magloughlin</h1>
+            <a href='magloughlin.zach@gmail.com'>magloughlin.zach@gmail.com</a>
+            <p>(For phone number, see resume.)</p>
+        </div>
+    )
+}
+
+export default pageHeader;
