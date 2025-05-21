45888785f166b32da5ca8f69a766d26845cde43e
ZachMc21 <magloughlin.zach@gmail.com>
2025-05-21 15:49:27 -0600

    Fixed babel error. Page is running successfully, but not displaying anything.

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ import './components/PageHeader.tsx';
 import React from 'react';
 import PageHeader from './components/PageHeader';
 
-function App() {
-    <div className="App">
-        <PageHeader />
-        {/*
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-            Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-            </a>
-        </header>
-    */}
-    </div>
+const App = () => {
+    return(
+        <div className="App">
+            <PageHeader />
+            {/*
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+        */}
+        </div>
+    )
 }
 
 export default App;
